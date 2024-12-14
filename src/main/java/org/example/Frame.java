105d3b92d6f8c5642d34ff11ccc8eff5969fdb10
sebastiangooddays <sebastiangooddays@gmail.com>
2024-12-15 01:50:17 +0800

    Background sound effect

--- src/main/java/org/example/Frame.java
+++ src/main/java/org/example/Frame.java
@@ -1,20 +1,31 @@
 package org.example;
 
+import javax.sound.sampled.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.File;
+import java.io.IOException;
 
 public class Frame extends JFrame implements KeyListener {
     JLabel label;
     JLabel label2;
+    Clip clip;
+    int score=0;
     ImageIcon icon=new ImageIcon("space_ship.png");
     ImageIcon image=new ImageIcon("moon.png");
-    Frame(){
+    Frame() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(600,600);
         this.getContentPane().setBackground(Color.BLACK);
         this.addKeyListener(this);
+
+        File file=new File("Gemini - The Soundlings.wav");
+        AudioInputStream audioStream= AudioSystem.getAudioInputStream(file);
+        clip=AudioSystem.getClip();
+        clip.open(audioStream);
+        clip.start();
 
         label2=new JLabel();
         label2.setIcon(image);
@@ -56,7 +67,8 @@ public class Frame extends JFrame implements KeyListener {
 //                break;
         }
         if(isColliding()){
-            JOptionPane.showMessageDialog(null, "YOU LANDED ON THE MOON :)");
+            score++;
+            JOptionPane.showMessageDialog(null, "YOU LANDED ON THE MOON :) "+score);
         }
     }
 
@@ -76,7 +88,9 @@ public class Frame extends JFrame implements KeyListener {
                 break;
         }
         if(isColliding()){
-            JOptionPane.showMessageDialog(null, "YOU LANDED ON THE MOON :)");
+            score++;
+            JOptionPane.showMessageDialog(null, "YOU LANDED ON THE MOON :) score: "+score);
+            label.setLocation(0,400);
 
     }
     }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,12 @@
 package org.example;
 
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import java.awt.*;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)throws UnsupportedAudioFileException, IOException, LineUnavailableException {
         new Frame();
     }
 }
