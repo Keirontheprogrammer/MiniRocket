e2d25bb7b3fa7ff216c3928b4c91f0e873c10fef
sebastiangooddays <sebastiangooddays@gmail.com>
2024-12-18 05:50:54 +0800

    enemy 2D graphic and new gameOver method

--- src/main/java/org/example/Frame.java
+++ src/main/java/org/example/Frame.java
@@ -8,7 +8,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Random;
 
-public class Frame extends JFrame implements MouseListener {
+public class Frame extends JFrame implements MouseListener,ActionListener{
     JLabel label;
     JLabel label2;
     JLabel label3;
@@ -17,18 +17,31 @@ public class Frame extends JFrame implements MouseListener {
     Action downAction;
     Action leftAction;
     Action rightAction;
-    int score=0;
     ImageIcon icon=new ImageIcon("space_ship.png");
     ImageIcon ship=new ImageIcon("901787.png");
     ImageIcon image=new ImageIcon("moon.png");
+    Image enemy;
     Random random=new Random();
+    int score=0;
+    int x=0;
+    int y=0;
+    int xVelocity=2;
+    int yVelocity=3;
+    boolean gameOver=false;
 
     Frame() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(600,600);
+        this.setLocationRelativeTo(null);
+
+        enemy=new ImageIcon("enemy.png").getImage();
+
 
         Timer timer=new Timer(400,e ->colorchange());
         timer.start();
+
+        Timer time=new Timer(10,this);
+        time.start();
 
 
         File file=new File("action-loop-e-90-bpm-brvhrtz-233462.wav");
@@ -43,7 +56,6 @@ public class Frame extends JFrame implements MouseListener {
         label3.setForeground(Color.WHITE);
         label3.setBackground(Color.BLACK);
         label3.setFont(new Font("MV Boli",Font.BOLD,13));
-//        label3.setOpaque(true);
         label3.setText("Click SpaceShip to change");
 
         label2=new JLabel();
@@ -72,11 +84,17 @@ public class Frame extends JFrame implements MouseListener {
         label.getInputMap().put(KeyStroke.getKeyStroke("RIGHT"),"RightKey");
         label.getActionMap().put("RightKey",rightAction);
 
+
         this.add(label);
         this.add(label2);
         this.add(label3);
         this.setLayout(null);
         this.setVisible(true);
+    }
+    public void paint(Graphics g){
+        super.paint(g);
+        Graphics2D g2D=(Graphics2D) g;
+        g2D.drawImage(enemy,x,y,null);
     }
 
     boolean isColliding(){
@@ -84,61 +102,20 @@ public class Frame extends JFrame implements MouseListener {
         return label.getBounds().intersects(label2.getBounds());
     }
 
+    boolean Colliding(){
+        Rectangle enemyRect=new Rectangle(x,y,enemy.getWidth(null),enemy.getHeight(null));
+        return label.getBounds().intersects(enemyRect);
+    }
 
-//    @Override
-//    public void keyTyped(KeyEvent e) {
-//        switch (e.getKeyChar()){
-//            case 'w':label.setLocation(label.getX(),label.getY()-10);
-//                break;
-//
-//            case 's':label.setLocation(label.getX(),label.getY()+10);
-//                break;
-//
-//            case 'a':label.setLocation(label.getX()-10,label.getY());
-//                break;
-//
-//            case 'd':label.setLocation(label.getX()+10,label.getY());
-//                break;
-//
-//            case :label.setLocation(label.getX()+10,label.getY()-10);
-//                break;
-//
-//            case :label.setLocation(label.getX()-10,label.getY()+10);
-//                break;
-//        }
-//        if(isColliding()){
-//            score++;
-//            JOptionPane.showMessageDialog(null, "YOU LANDED ON THE MOON :) "+score);
-//        }
-//    }
+    public void resetGame(){
+        gameOver=false;
+        label.setLocation(0,400);
+        x=180;
+        y=0;
+        repaint();
 
-//    @Override
-//    public void keyPressed(KeyEvent e) {
-//        switch (e.getKeyCode()){
-//            case 38:label.setLocation(label.getX(),label.getY()-10);
-//                break;
-//
-//            case 40:label.setLocation(label.getX(),label.getY()+10);
-//                break;
-//
-//            case 37:label.setLocation(label.getX()-10,label.getY());
-//                break;
-//
-//            case 39:label.setLocation(label.getX()+10,label.getY());
-//                break;
-//        }
-//        if(isColliding()){
-//            score++;
-//            JOptionPane.showMessageDialog(null, "YOU LANDED ON THE MOON :) score: "+score);
-//            label.setLocation(0,400);
-//
-//    }
-//    }
+    }
 
-//    @Override
-//    public void keyReleased(KeyEvent e) {
-//
-//    }
 public void colorchange(){
         int r=random.nextInt(56);
         int g=random.nextInt(26);
@@ -149,6 +126,23 @@ public void colorchange(){
 
 }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(!gameOver){
+            if(x>=600-enemy.getWidth(null)||x<0){
+                xVelocity=xVelocity*-1;
+            }
+            x += xVelocity;
+            if(y>=600-enemy.getHeight(null)||y<0){
+                yVelocity=yVelocity*-1;
+            }
+            y += yVelocity;
+            repaint();
+        }
+    }
+
+
+
     public class UpAction extends AbstractAction{
 
         @Override
@@ -158,6 +152,15 @@ public void colorchange(){
                 score++;
                 JOptionPane.showMessageDialog(null,"LANDED :) score: "+score);
                 label.setLocation(0,400);
+                resetGame();
+
+            }
+
+            if(Colliding()){
+                gameOver=true;
+                score--;
+                JOptionPane.showMessageDialog(null,"GAME OVER :( score: "+score);
+                resetGame();
             }
 
         }
@@ -172,6 +175,13 @@ public void colorchange(){
                 score++;
                 JOptionPane.showMessageDialog(null,"LANDED :) score: "+score);
                 label.setLocation(0,400);
+                resetGame();
+            }
+            if(Colliding()){
+                gameOver=true;
+                score--;
+                JOptionPane.showMessageDialog(null,"GAME OVER :( score: "+score);
+                resetGame();
             }
         }
     }
@@ -185,6 +195,13 @@ public void colorchange(){
                 score++;
                 JOptionPane.showMessageDialog(null,"LANDED :) score: "+score);
                 label.setLocation(0,400);
+                resetGame();
+            }
+            if(Colliding()){
+                gameOver=true;
+                score--;
+                JOptionPane.showMessageDialog(null,"GAME OVER :( score: "+score);
+                resetGame();
             }
         }
     }
@@ -198,11 +215,17 @@ public void colorchange(){
                 score++;
                 JOptionPane.showMessageDialog(null,"LANDED :) score: "+score);
                 label.setLocation(0,400);
+                resetGame();
+            }
+            if(Colliding()){
+                gameOver=true;
+                score--;
+                JOptionPane.showMessageDialog(null,"GAME OVER :( score: "+score);
+                resetGame();
             }
         }
 
     }
-
 
 
 
